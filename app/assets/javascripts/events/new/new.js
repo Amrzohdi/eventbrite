35e59d5017d069643226fe9db723b78341fa6450
Amr Essam <amr.essam67@gmail.com>
2018-08-10 17:17:20 +0200

    fully implementation for event creation and show

--- app/assets/javascripts/events/new/new.js
+++ app/assets/javascripts/events/new/new.js
@@ -3,31 +3,42 @@ $(function () {
         return;
 
     drawGoogleMap();
-});
 
 
-function drawGoogleMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 17
-    });
 
-    var marker;
-
-    if(navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(displayOnMap);
-
-    function displayOnMap(position) {
-        var position =  new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: 'Event Location'
+    function drawGoogleMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 17
         });
-        map.setCenter(position);
+
+        var marker;
+
+        if(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(displayOnMap);
+
+        function displayOnMap(position) {
+            setFormLocation(position.coords.latitude, position.coords.longitude);
+            var position =  new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: 'Event Location'
+            });
+            map.setCenter(position);
+
+        }
+
+        map.addListener('click', function(event) {
+            var position = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+            marker.setPosition(position);
+            setFormLocation(event.latLng.lat(),event.latLng.lng());
+        });
     }
 
-    map.addListener('click', function(event) {
-        var position = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
-        marker.setPosition(position);
-    });
-}
+    function setFormLocation(lat,lng) {
+        $('#longitude_input').val(lng);
+        $('#latitude_input').val(lat);
+
+    }
+});
+
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /events
   # GET /events.json
@@ -24,15 +25,15 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = Event.new(event_params.merge(user: current_user))
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
+        format.html {redirect_to @event, notice: 'Event was successfully created.'}
+        format.json {render :show, status: :created, location: @event}
       else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @event.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +43,11 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
+        format.html {redirect_to @event, notice: 'Event was successfully updated.'}
+        format.json {render :show, status: :ok, location: @event}
       else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @event.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,19 +57,20 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to events_url, notice: 'Event was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.fetch(:event, {})
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.require(:event).permit(:name, :longitude, :latitude, :start_date, :end_date)
+  end
+
 end
