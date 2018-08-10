721f3a27161a57a18870fc64abad0c93b77bea72
Amr Essam <amr.essam67@gmail.com>
2018-08-10 16:05:27 +0200

    map centered with current location

--- app/assets/javascripts/events/new/new.js
+++ app/assets/javascripts/events/new/new.js
@@ -3,7 +3,14 @@ $(function () {
         return;
 
     handler = Gmaps.build('Google');
-    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+
+    handler.buildMap({ provider: { zoom:15}, internal: {id: 'map'}}, function(){
         handler.fitMapToBounds();
+        if(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(displayOnMap);
     });
+
+    function displayOnMap(position) {
+        handler.map.centerOn(new google.maps.LatLng(position.coords.latitude, position.coords.longitude) );
+    }
 });
